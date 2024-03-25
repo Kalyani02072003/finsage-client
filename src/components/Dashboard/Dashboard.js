1e386d6693638de07df772a865fd0ab6974128ab
Kalyani Patra <71889838+Kalyani02072003@users.noreply.github.com>
2024-03-25 21:32:40 +0530

    feat: fixed dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -7,51 +7,49 @@ import RecommendationContainer from '../HomePage/Recommendation/Recommendation';
 import StockCard from './StockCard';
 
 function Dashboard() {
-    const symbols=[
+    const symbols = [
         ["APPLE", "AAPL"],
         ["GOOGLE", "GOOGL"],
         ["MICROSOFT", "MSFT"],
         ["AMAZON", "AMZN"],
         ["FACEBOOK", "FB"],
-        ["TESLA", "TSLA"], 
+        ["TESLA", "TSLA"],
         ["AMD", "AMD"],
-        ["NASDAQ", "NDAQ"]
+        ["NASDAQ", "NDAQ"],
+        ["BOEING", "BA"],
+        ["REDDIT", "RDDT"],
     ];
 
-    return (
-        <div>
-            <Navbar />
-            <TickerTape colorTheme="dark" />
-            <Typography variant="h5" align="center" style={{ marginTop: '20px' }}>
-                TRACK INVESTMENTS YOU CARE ABOUT HERE
-            </Typography>
-            <Grid container spacing={3} justify="center" style={{ marginTop: '20px', maxWidth: '100%', overflowX: 'hidden' }}>
-                <Grid item xs={12} md={10} lg={9} xl={10}>
-                    <Grid container spacing={1}>
-                        {/* Left Column */}
-                        <Grid item xs={12} md={6} style={{marginTop:'20px'}}>
-                            <SymbolOverview
-                                colorTheme="dark"
-                                width='100%'
-                                symbols={symbols}
-                                chartType="candlesticks"
-                                downColor="#800080"
-                                borderDownColor="#800080"
-                                wickDownColor="#800080"
-                                height={600}
-                            />
-                        </Grid>
-                        {/* Right Column */}
-                        <Grid item xs={12} md={6}>
-                            {/* Recommendation Component */}
-                            <RecommendationContainer />
-                        </Grid>
-                    </Grid>
-                </Grid>
+  return (
+    <div>
+        <Navbar />
+        <TickerTape colorTheme="dark" />
+        <Typography variant="h5" align="center" style={{ marginTop: '20px' }}>
+            TRACK INVESTMENTS YOU CARE ABOUT HERE
+        </Typography>
+        <Grid container spacing={2} style={{ marginTop: '20px', paddingLeft:'80px' }}>
+            {/* Left Column - SymbolOverview */}
+            <Grid item xs={12} md={6} style={{marginTop:'16px'}}>
+            <SymbolOverview
+                colorTheme="dark"
+                width='100%'
+                symbols={symbols}
+                chartType="candlesticks"
+                downColor="#800080"
+                borderDownColor="#800080"
+                wickDownColor="#800080"
+                height={600}
+                
+            />
             </Grid>
-            <StockCard />
-        </div>
-    );
+            {/* Right Column - RecommendationContainer */}
+            <Grid item xs={12} md={6}>
+            <RecommendationContainer />
+            </Grid>
+      </Grid>
+      <StockCard />
+    </div>
+  );
 }
 
 export default Dashboard;
